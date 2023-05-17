e78e274820ad9e7e00eade5bc6f7696f62595449
Nguyenletrungthien <101155715+Nguyenletrungthien@users.noreply.github.com>
2023-05-17 20:01:45 +0700

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chị tha thứ cho đứa em này nháaaaaaaaa!!!",
+  descriptionHeader:
+    "Nếu chị thoát ra và không trả lời thì có nghĩa là chị đã ĐỒNG Ý rồi đấy ^^",
+  buttonYes: "Đồng Ý",
+  buttonNo: "Không! Còn lâu nhé.",
+  titleModar: "Cảm ơn chị vì đồng ý",
+  descriptionModar:
+    'Em biết là chị sẽ đồng ý mà ^^ </br> Cảm ơn chị vì đã đồng ý tha thứ cho em nhá. Đối với em, chị mãi là người em thương nhất :)) </br>  Em thương nhiều đến mấy, chị sẽ hiểu thôi "',
+};
